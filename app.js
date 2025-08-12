2a8d4ca017a6a17e3601e403b993a2d99cb504f0
Juan Diego Soto <156706569+sotus10@users.noreply.github.com>
2025-08-12 09:03:46 -0600

    Primeras partes en JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+function cambiarContenido(elemento,texto) {
+    document.getElementById(elemento)
+    elemento.
+}
